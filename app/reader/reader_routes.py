42d8cde59184733d7ced6ec24f8ed77f5ae38d54
Brett Burley-Inners <b.m.burleyinners@gmail.com>
2020-07-20 18:56:26 -0400

    Added Marko so posts can be written and saved in Markdown.

--- app/reader/reader_routes.py
+++ app/reader/reader_routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from sqlalchemy import func, desc
+import marko
 
 from . import reader_bp
 from app import db, models
@@ -13,10 +14,18 @@ def reader_home():
 
 @reader_bp.route("/post/<int:post_id>")
 def read_post(post_id):
-    post = models.Post._get_post(post_id)
-    other_posts = models.Post.query.filter(models.Post.post_id != post.post_id).order_by(func.random()).limit(5)
+    post_from_db = models.Post._get_post(post_id)
+    other_posts = models.Post.query.filter(models.Post.post_id != post_from_db.post_id).order_by(func.random()).limit(5)
     
-    print(other_posts)
-    print(post)
+    post = {}
+    post["post_id"] = post_from_db.post_id
+    post["post_title"] = post_from_db.post_title
+    post["post_subtitle"] = post_from_db.post_subtitle
+    post["post_date"] = post_from_db.post_date
+    post["post_content"] = marko.convert(post_from_db.post_content)
+
+    #parsed_html = marko.convert(post.post_content)
+    #post.update(marko.convert({ "parsed_html": post.post_content }))
+    #post["parsed_html"] = marko.convert(post.post_content)
 
     return render_template("reader/reader_view_post.html", post=post, other_posts=other_posts)
