6392f285d007358e6ae74a8352596d69891b43a5
Brett Burley-Inners <b.m.burleyinners@gmail.com>
2020-07-20 21:18:12 -0400

    Added preview button (needs styling applied) and fixed reader home to display Markdown.

--- app/reader/reader_routes.py
+++ app/reader/reader_routes.py
@@ -8,7 +8,19 @@ from app import db, models
 
 @reader_bp.route("/")
 def reader_home():
-    posts = models.Post.query.order_by(desc(models.Post.post_date)).limit(4)
+    posts_from_db = models.Post.query.order_by(desc(models.Post.post_date)).limit(4)
+
+    posts = []
+    for p in posts_from_db:
+        post = {}
+        post["post_id"] = p.post_id
+        post["post_title"] = p.post_title
+        post["post_subtitle"] = p.post_subtitle
+        post["post_date"] = p.post_date
+        post["post_content"] = marko.convert(p.post_content)
+
+        posts.append(post)
+    
     return render_template("reader/home.html", posts=posts)
 
 
@@ -23,9 +35,5 @@ def read_post(post_id):
     post["post_subtitle"] = post_from_db.post_subtitle
     post["post_date"] = post_from_db.post_date
     post["post_content"] = marko.convert(post_from_db.post_content)
-
-    #parsed_html = marko.convert(post.post_content)
-    #post.update(marko.convert({ "parsed_html": post.post_content }))
-    #post["parsed_html"] = marko.convert(post.post_content)
 
     return render_template("reader/reader_view_post.html", post=post, other_posts=other_posts)
