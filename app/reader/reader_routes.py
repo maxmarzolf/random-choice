e23675b0551ef939459e45d852f0eb118debd764
maxmarzolf <max@treesfromspace.com>
2020-11-01 13:51:02 -0500

    just getting it to run, will debug later

--- app/reader/reader_routes.py
+++ app/reader/reader_routes.py
@@ -8,27 +8,27 @@ from app import db, models, login_manager
 
 @reader_bp.route("/")
 def reader_home():
-    posts_from_db = models.Post.query.order_by(desc(models.Post.post_date)).limit(4)
-
-    posts = []
-    for p in posts_from_db:
-        post = {}
-        post["post_id"] = p.post_id
-        post["post_title"] = p.post_title
-        post["post_subtitle"] = p.post_subtitle
-        post["post_date"] = p.post_date
-        post["post_content"] = marko.convert(p.post_content)
-
-        posts.append(post)
-    
-    return render_template("reader/home.html", posts=posts)
+    # posts_from_db = models.Post.query.order_by(desc(models.Post.post_date)).limit(4)
+    #
+    # posts = []
+    # for p in posts_from_db:
+    #     post = {}
+    #     post["post_id"] = p.post_id
+    #     post["post_title"] = p.post_title
+    #     post["post_subtitle"] = p.post_subtitle
+    #     post["post_date"] = p.post_date
+    #     post["post_content"] = marko.convert(p.post_content)
+    #
+    #     posts.append(post)
+    #
+    return render_template("reader/home.html")
 
 
 @reader_bp.route("/post/<int:post_id>")
 def read_post(post_id):
     post_from_db = models.Post._get_post(post_id)
     other_posts = models.Post.query.filter(models.Post.post_id != post_from_db.post_id).order_by(func.random()).limit(5)
-    
+
     post = {}
     post["post_id"] = post_from_db.post_id
     post["post_title"] = post_from_db.post_title
