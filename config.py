1e198eb5e61cfcf283ed79667873c59879c8fa0b
maxmarzolf <max@treesfromspace.com>
2020-11-02 13:23:06 -0500

    slight title and font change

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from datetime import timedelta
 basedirectory = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedirectory, '.env'))
 
-class Config():
+
+class Config:
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = environ.get("SECRET_KEY")
