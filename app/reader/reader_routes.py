8a1bad77e2a51283a0ae3de3177170eb044d0003
maxmarzolf <max@treesfromspace.com>
2020-11-11 15:02:12 -0500

    I like your way too, just wanted to feel like I'm adding value hahahaha

--- app/reader/reader_routes.py
+++ app/reader/reader_routes.py
@@ -15,11 +15,7 @@ def reader_home():
 
     posts = []
     for p in posts_from_db:
-        post = {}
-        post["post_id"] = p.ID
-        post["post_title"] = p.TITLE
-        post["post_subtitle"] = p.SUBTITLE
-        post["post_date"] = p.POSTED_DATE
+        post = {"post_id": p.ID, "post_title": p.TITLE, "post_subtitle": p.SUBTITLE, "post_date": p.POSTED_DATE}
         print(type(p.POSTED_DATE))
         #post["post_content"] = marko.convert(p.post_content)
         post["post_content"] = p.CONTENT_HTML
@@ -35,12 +31,8 @@ def read_post(post_id):
     #other_posts = models.Article.query.filter(models.Post.post_id != post_from_db.post_id).order_by(func.random()).limit(5)
     other_posts = models.Article.get_random_articles(5)
 
-    post = {}
-    post["post_id"] = post_from_db.ID
-    post["post_title"] = post_from_db.TITLE
-    post["post_subtitle"] = post_from_db.SUBTITLE
-    post["post_date"] = post_from_db.POSTED_DATE
+    post = {"post_id": post_from_db.ID, "post_title": post_from_db.TITLE, "post_subtitle": post_from_db.SUBTITLE,
+            "post_date": post_from_db.POSTED_DATE, "post_content": post_from_db.CONTENT_HTML}
     #post["post_content"] = marko.convert(post_from_db.post_content)
-    post["post_content"] = post_from_db.CONTENT_HTML
 
     return render_template("reader/reader_view_post.html", post=post, other_posts=other_posts)
