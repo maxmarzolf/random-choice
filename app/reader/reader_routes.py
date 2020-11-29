d3f51d67924c991b4c38fe1aaf65a1a19fe07921
maxmarzolf <max@treesfromspace.com>
2020-11-29 17:33:56 -0500

    I think this was left in for testing and wasn't removed, also increased specificity of exception

--- app/reader/reader_routes.py
+++ app/reader/reader_routes.py
@@ -1,14 +1,11 @@
 from flask import render_template, flash
 from sqlalchemy import exc
-
 from . import reader_bp
 from app import models
 
 
 @reader_bp.route("/")
 def reader_home():
-    raise exc.SQLAlchemyError
-
     try:
         posts_from_db = models.Article.get_random_articles(4)
         posts = []
@@ -16,22 +13,17 @@ def reader_home():
             post = {"post_id": p.id, "post_title": p.title, "post_subtitle": p.subtitle, "post_date": p.posted_date}
             print(type(p.posted_date))
             post["post_content"] = p.content_html
-            
             posts.append(post)
-        
         return render_template("reader/home.html", posts=posts)
-    except:
+    except exc.SQLAlchemyError:
         flash('Could not retrieve posts.', 'error')
         return render_template("reader/home.html", posts=[])
 
 
 @reader_bp.route("/post/<int:post_id>")
 def read_post(post_id):
-
     post_from_db = models.Article.get_article(post_id)
     other_posts = models.Article.get_random_articles(5)
-
     post = {"post_id": post_from_db.id, "post_title": post_from_db.title, "post_subtitle": post_from_db.subtitle,
             "post_date": post_from_db.posted_date, "post_content": post_from_db.content_html}
-
     return render_template("reader/reader_view_post.html", post=post, other_posts=other_posts)
