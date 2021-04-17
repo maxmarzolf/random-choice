8a1cf76a630cc60e63efaccba898b61266fab767
maxmarzolf <max@treesfromspace.com>
2021-04-17 17:36:05 -0400

    SECRET_KEY removed

--- config.py
+++ config.py
@@ -7,18 +7,21 @@ load_dotenv(path.join(base_directory, '../../.env'))
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # TODO: is SECRET_KEY used anymore?
-    SECRET_KEY = environ.get("SECRET_KEY")
     USE_SESSION_FOR_NEXT = True
-    TESTING = False
 
 
 class Development(Config):
     SQLALCHEMY_DATABASE_URI = environ.get("DEV_DATABASE_URI")
     FLASK_ENV = 'development'
+    TESTING = False
 
 
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
     FLASK_ENV = 'production'
     DEBUG = False
+    TESTING = False
+
+
+class Testing(Config):
+    TESTING = True
