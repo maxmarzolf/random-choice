7ad516e1cfc792832af5e064daf53293b1ba1f90
maxmarzolf <max@treesfromspace.com>
2021-04-21 20:29:52 -0400

    test configuration started

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,39 +1,19 @@
-import os
-import tempfile
 import pytest
+
 from app import create_app
 
 
-with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
-    _data_sql = f.read().decode("utf8")
-
-
 @pytest.fixture
-def app():
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
-    app = create_app({"TESTING": True, "DATABASE": db_path})
-
-    # create the database and load test data
-    with app.app_context():
-        # init_db()
-        # get_db().executescript(_data_sql)
-        print("running tests.")
-
-    yield app
-
-    # close and remove the temporary database
-    os.close(db_fd)
-    os.unlink(db_path)
+def client():
+    app = create_app()
+    app.testing = True
+    client = app.test_client()
+    yield client
 
 
-@pytest.fixture
-def client(app):
-    """A test client for the app."""
-    return app.test_client()
+def test_home(client):
+    assert client.get('/').status_code == 200
 
 
-def test_bad_url(client):
-    rv = client.get("/something")
-    assert b'Not Found' in rv.data
+def test_me(client):
+    assert client.get('/post/1').status_code == 200
