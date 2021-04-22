92ad313bebd8c352fd390a6927f11eb3ad3f6b1e
maxmarzolf <max@treesfromspace.com>
2021-04-21 20:45:35 -0400

    renamed test function, file structure reformat

--- tests/create_client.py
+++ tests/create_client.py
@@ -9,11 +9,3 @@ def client():
     app.testing = True
     client = app.test_client()
     yield client
-
-
-def test_home(client):
-    assert client.get('/').status_code == 200
-
-
-def test_me(client):
-    assert client.get('/post/1').status_code == 200
